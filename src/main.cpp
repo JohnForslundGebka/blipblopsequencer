de0e77642d06527ad3e9ddf9c5fdf701cce8c710
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-03-01 11:37:29 +0100

    Create buttonladder object

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 #include "pitches.h"
 #include "buttonLadder.h"
+#include "Button.h"
 
-
+ButtonLadder buttonLadder(0);
 
 void setup() {
 
-
+ Serial.begin(9600);
 }
 
 void loop() {
 
+  uint8_t value = buttonLadder.readButtonLadder();
+
+  Serial.println(value);
+
+  delay(100);
 
 }
