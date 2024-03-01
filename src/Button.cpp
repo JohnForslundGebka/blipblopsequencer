6be2aca5b2e371279a680f316b2fe65e194f7011
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-03-01 11:22:03 +0100

    add member functions

---
+++ src/Button.cpp
@@ -0,0 +1,19 @@
+#include "button.h"
+
+bool Button::readButton()
+{
+    bool reading = digitalRead(m_digitalPin);
+
+    if (reading != m_lastButtonState){
+        m_lastDebounceTime = millis();
+    }
+
+    if ((millis() - m_lastDebounceTime) > m_debounceTime) {
+        if (reading != m_buttonState) {
+            m_buttonState = reading;
+        }
+    }
+
+    m_lastButtonState = reading;
+    return m_buttonState;
+}
