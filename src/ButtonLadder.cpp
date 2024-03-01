70da842685529f97d834dad19e79c11d1e83c9fb
cyberhelmer <petterson.melker@gmail.com>
2024-03-01 16:50:48 +0100

    Deleted an unneccecary if-statement

--- src/ButtonLadder.cpp
+++ src/ButtonLadder.cpp
@@ -7,31 +7,30 @@ uint8_t ButtonLadder::buttonLadder()
     //reads value from the resistor ladder, only used privately for the read() function.
     int buttonReadValue = analogRead(m_pinNumber);
 
+    
     if (buttonReadValue > 100)
     {
-        if (buttonReadValue > 100)
-        {
-            if(buttonReadValue > 100 && buttonReadValue < 200) {
-                return  1;
-            } else if (buttonReadValue > 200 && buttonReadValue < 300) {
-                return  2;
-            } else if (buttonReadValue > 300 && buttonReadValue < 400) {
-                return  3;
-            } else if (buttonReadValue > 400 && buttonReadValue < 550) {
-                return  4;
-            } else if (buttonReadValue > 550 && buttonReadValue < 700) {
-                return  5;
-            } else if (buttonReadValue > 700 && buttonReadValue < 800) {
-                return  6;
-            } else if (buttonReadValue > 800 && buttonReadValue < 900) {
-                return  7;
-            } else if(buttonReadValue > 850) {
-                return  8;
-            }
+        if(buttonReadValue > 100 && buttonReadValue < 200) {
+            return  1;
+        } else if (buttonReadValue > 200 && buttonReadValue < 300) {
+            return  2;
+        } else if (buttonReadValue > 300 && buttonReadValue < 400) {
+            return  3;
+        } else if (buttonReadValue > 400 && buttonReadValue < 550) {
+            return  4;
+        } else if (buttonReadValue > 550 && buttonReadValue < 700) {
+            return  5;
+        } else if (buttonReadValue > 700 && buttonReadValue < 800) {
+            return  6;
+        } else if (buttonReadValue > 800 && buttonReadValue < 900) {
+            return  7;
+        } else if(buttonReadValue > 850) {
+            return  8;
         }
-        else
-            return 0;
     }
+    else
+        return 0;
+    
 }
 
 //Reads the button ladder with debounce and returns the button number. No button pressed returns 0.
