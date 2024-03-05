0d2df4458b3c5980a32adccb62ef4fa1269ba832
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-03-05 12:04:35 +0100

    remove old variables

--- include/Button.h
+++ include/Button.h
@@ -6,18 +6,16 @@
 class Button
 {
 private:
+
     const unsigned int m_digitalPin;
-    uint8_t m_buttonState;
-    uint8_t m_lastButtonState;
+
+
 
 
     unsigned long m_lastDebounceTime = 0;
     unsigned long m_debounceTime = 10;
 
-    // Define the interval for a short press
-    const unsigned int SHORT_PRESS_MIN = 10;  // Minimum time for short press in milliseconds
-    const unsigned int SHORT_PRESS_MAX = 400; // Maximum time for short press in milliseconds
-    unsigned int m_buttonPressTime;
+
 public:
     //Constructor
     Button(int unsigned digitalPin) : m_digitalPin(digitalPin)
